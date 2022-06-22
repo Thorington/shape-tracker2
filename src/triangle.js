1eede91e88c99a602427ccd895b06337f109630b
Claire Thorington <claire.thorington@gmail.com>
2022-06-22 14:15:44 -0700

    add final files and Caroline as a contributor co-authored-by: Caroline Cerussi <caroceru@gmail.com>

--- src/triangle.js
+++ src/triangle.js
@@ -1,6 +1,6 @@
 //Business Logic
 
-function Triangle(side1, side2, side3) {
+export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
@@ -8,7 +8,7 @@ function Triangle(side1, side2, side3) {
 
 Triangle.prototype.checkType = function() {
   return "I can't answer that yet!";
-}
+};
 
 //UI Logic
 
@@ -18,7 +18,7 @@ $(document).ready(function() {
     const length1 = $('#length1').val();
     const length2 = $('#length2').val();
     const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3)
+    const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
   });
