b0b1477c40479434da292040461864179153b947
Claire Thorington <claire.thorington@gmail.com>
2022-06-24 08:00:30 -0700

    update circle tests

--- __tests__/circle.test.js
+++ __tests__/circle.test.js
@@ -9,12 +9,12 @@ describe('Circle', () => {
 
   test('should correctly determine the diameter of a circle given its radius', () => {
     const circle = new Circle(2);
-    expect(circle.toDiameter()).toEqual(circle.radius*2);
+    expect(circle.toDiameter()).toEqual(4);
   })
 
   test('should correctly determine the circumference of a circle given its radius', () => {
     const circle = new Circle(2);
-    expect(circle.toCircumference()).toEqual((circle.radius*2)*3.14);
+    expect(circle.toCircumference()).toEqual(12.56);
   })
 
 });
